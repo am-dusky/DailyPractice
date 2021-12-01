f57609785526efa938145f96857d78b768d3a75f
Megha Singh <81347179+am-dusky@users.noreply.github.com>
2021-12-01 11:56:33 +0530

    Create validateBST.cpp

---
+++ dp/validateBST.cpp
@@ -0,0 +1,10 @@
+ bool solve(TreeNode*root,long minVal,long maxVal){
+        if(root==NULL)return true;
+        if(root->val >=maxVal || root->val<=minVal)
+            return false;
+        return solve(root->left,minVal,root->val)&& solve(root->right,root->val,maxVal);
+    }
+    
+    bool isValidBST(TreeNode* root) {
+        return solve(root,LONG_MIN,LONG_MAX);
+    }
