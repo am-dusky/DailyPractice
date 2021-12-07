49c83a3d1524d9109ec2a1ff64720899ebb4dbe6
Megha Singh <81347179+am-dusky@users.noreply.github.com>
2021-12-07 21:26:02 +0530

    Create 1217.cpp

---
+++ greedy/1217.cpp
@@ -0,0 +1,10 @@
+ int minCostToMoveChips(vector<int>& position) {
+        int odd=0 ,even=0 ;
+        for(int i=0;i<position.size();i++){
+            if(position[i]%2)
+                odd++ ;
+            else
+                even++ ;
+        }
+        return min(odd , even) ;
+    }
